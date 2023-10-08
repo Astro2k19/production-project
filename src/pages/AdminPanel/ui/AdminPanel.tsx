993c621ca1b0d00db9b05b52a777a09f13721b2b
Astro2k19 <kartem200351@gmail.com>
2023-10-08 15:39:29 +0300

    remove the non-existent import

--- src/pages/AdminPanel/ui/AdminPanel.tsx
+++ src/pages/AdminPanel/ui/AdminPanel.tsx
@@ -4,8 +4,6 @@ import { useTranslation } from 'react-i18next'
 import { classNames } from '@/shared/lib'
 import { Page } from '@/widgets/Page'
 
-import cls from './AdminPanel.module.scss'
-
 interface AdminPanelProps {
   className?: string
 }
@@ -13,7 +11,7 @@ interface AdminPanelProps {
 const AdminPanel: FC<AdminPanelProps> = ({ className }) => {
   const { t } = useTranslation()
   return (
-      <Page className={classNames([cls.adminPanel, className])} dataTestId={'AdminPage'}>
+      <Page className={classNames([className])} dataTestId={'AdminPage'}>
           <h1>{t('Admin panel')}</h1>
       </Page>
   )
